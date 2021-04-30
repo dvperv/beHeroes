f5c9e09badb9d99e9788370d3642065f6b142098
DVPervukhin <dvperv@gmail.com>
2021-04-30 16:41:50 +0300

    Atlas connection added

--- src/server.js
+++ src/server.js
@@ -7,9 +7,6 @@ const { Schema } = mongoose;
 const config = require('./config');
 const atlas_uri = `mongodb+srv://${config.atlas.username}:${config.atlas.password}@cluster0.vx1qm.mongodb.net/${config.atlas.db}?retryWrites=true&w=majority`;
 
-const heroModel = mongoose.model('hero', new Schema({ name: String }));
-heroModel.create({name: 'test'});
-
 try {
     mongoose.connect(atlas_uri, {useNewUrlParser: true, useUnifiedTopology: true});
     console.log('dp: atlas connected');
@@ -17,6 +14,9 @@ try {
 catch (e) {
     console.log('dp: ERROR: CAN\'T CONNECT TO ATLAS: ' + e);
 }
+
+const heroModel = mongoose.model('hero', new Schema({ name: String }));
+heroModel.create({name: 'test'});
 
 fastify.register(require('fastify-cors'), {
     origin: '*',
